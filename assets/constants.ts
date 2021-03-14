e6e41921931af6755fe628eb73e1dd03c0dc77dd
Daniel Sobczak <danielsobczi@gmail.com>
2021-03-14 18:25:24 +0100

    chore: auto build

--- assets/constants.ts
+++ assets/constants.ts
@@ -2,5 +2,6 @@ const ICONS_PATH = '/assets/icons'
 
 export const ICONS = [
   { name: 'login', path: `${ICONS_PATH}/login-24px.svg` },
-  { name: 'logout', path: `${ICONS_PATH}/exit_to_app-24px.svg` }
+  { name: 'logout', path: `${ICONS_PATH}/exit_to_app-24px.svg` },
+  { name: 'live_tv', path: `${ICONS_PATH}/live_tv-24px.svg` }
 ]
