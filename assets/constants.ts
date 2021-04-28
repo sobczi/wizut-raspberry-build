644bf91756333df30f94c1d04f4eeb62f3796039
Daniel Sobczak <danielsobczi@gmail.com>
2021-04-29 01:16:04 +0200

    chore: auto build

--- assets/constants.ts
+++ assets/constants.ts
@@ -3,5 +3,11 @@ const ICONS_PATH = '/assets/icons'
 export const ICONS = [
   { name: 'login', path: `${ICONS_PATH}/login-24px.svg` },
   { name: 'logout', path: `${ICONS_PATH}/exit_to_app-24px.svg` },
-  { name: 'live_tv', path: `${ICONS_PATH}/live_tv-24px.svg` }
+  { name: 'live_tv', path: `${ICONS_PATH}/live_tv-24px.svg` },
+  { name: 'photo_library', path: `${ICONS_PATH}/photo_library.svg` },
+  { name: 'calendar', path: `${ICONS_PATH}/calendar_today_black_24dp.svg` },
+  { name: 'temperature', path: `${ICONS_PATH}/thermostat_black_24dp.svg` },
+  { name: 'refresh', path: `${ICONS_PATH}/refresh_black_24dp.svg` },
+  { name: 'chevron', path: `${ICONS_PATH}/chevron_right_black_24dp.svg` },
+  { name: 'play_video', path: `${ICONS_PATH}/play_circle_black_24dp.svg` }
 ]
